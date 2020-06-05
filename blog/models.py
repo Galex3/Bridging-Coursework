62e69fb88470af5c35e8983cdb20b4a8aa9a2076
Gustavo Silva <gas823@student.bham.ac.uk>
2020-06-05 18:41:27 +0100

    First project commit

---
+++ blog/models.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+STATUS = (
+    (0,"Draft"),
+    (1,"Published")
+)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.status = 1
+        self.save()
+
+    def __str__(self):
+        return self.title
