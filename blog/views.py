e018e8ee2d8c200ae96fbb1475cd4ee9dc15467e
Gustavo Silva <gas823@student.bham.ac.uk>
2020-06-08 02:43:12 +0100

    Implemented Pagination

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-published_date')
     template_name = 'index.html'
+    paginate_by = 4
 
 
 class PostDetail(generic.DetailView):
