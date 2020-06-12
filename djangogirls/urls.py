f258ee460ab64eef17c4200ac6c4c7add9f0dfbc
Gustavo Silva <gas823@student.bham.ac.uk>
2020-06-12 23:20:27 +0100

    Didn't fix...

--- djangogirls/urls.py
+++ djangogirls/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import RedirectView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
-    re_path(r'^favicon\.png$', RedirectView.as_view(url='/static/favicon.png', permanent=True)),
 ]
