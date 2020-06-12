b2dca5e42458746522106eb8256b63aa7e2e2645
Gustavo Silva <gas823@student.bham.ac.uk>
2020-06-12 23:17:08 +0100

    Maybe fixed favicon on production version?

--- djangogirls/urls.py
+++ djangogirls/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
+    re_path(r'^favicon\.png$', RedirectView.as_view(url='/static/favicon.png', permanent=True)),
 ]
