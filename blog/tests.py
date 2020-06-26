4892796091f2be2c98792455970ec7b63ddd51a9
Gustavo Silva <gas823@student.bham.ac.uk>
2020-06-26 16:39:11 +0100

    Wrote a small Django test to test post creation. Passes all assertions

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from blog.models import Post
 
-# Create your tests here.
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='test', email='test@bham.ac.uk', password='top_secret')
+        Post.objects.create(author=self.user, title="TestCase1", content="Content test 1")
+
+    def test_post_creation(self):
+        test1 = Post.objects.get(title="TestCase1")
+        self.assertEqual(test1.status, 0)
+        self.assertEqual(test1.content, 'Content test 1')
+        self.assertEqual(test1.title, 'TestCase1')
