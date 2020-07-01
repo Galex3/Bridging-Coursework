750bd43a54b2f195f3d1e3278297f15ec7217126
Gustavo Silva <gas823@student.bham.ac.uk>
2020-07-01 18:21:43 +0100

    Wrote a few more tests for the Models

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from blog.models import Post, Item
+from blog.models import Post, CVExperience, HardSkill, SoftSkill, Project, Interest, Language, Education
 
 
 class PostTestCase(TestCase):
@@ -24,39 +24,76 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'base.html')
 
 
-class CVPageTest(TestCase):
+class CVModelTest(TestCase):
 
-    def test_can_save_a_POST_request(self):
-        self.client.post('/cv', data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/cv', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        response = self.client.get('/cv')
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
-
-
-class ItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
+    def test_create_and_read_education(self):
+        first_item = Education()
+        first_item.degree = 'Test'
+        first_item.institution = 'Inst Test'
+        first_item.duration = '2020'
+        first_item.item1 = 'Coursework 1'
+        first_item.item1_grade = 99.99
+        first_item.item2 = 'Coursework 2'
+        first_item.item2_grade = 80
+        first_item.item2 = 'Coursework 3'
+        first_item.item2_grade = 45.44
         first_item.save()
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.save()
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item.text, 'Item the second')
+        saved_items = Education.objects.all()
+        self.assertEqual(saved_items.count(), 1)
+        self.assertEqual(saved_items[0].institution, 'Inst Test')
+
+    def test_create_and_read_experience(self):
+        first_item = CVExperience()
+        first_item.role = 'Test'
+        first_item.company = 'Company'
+        first_item.duration = '2020'
+        first_item.item1 = 'Item 1'
+        first_item.item2 = 'Item 2'
+        first_item.item2 = 'Item 3'
+        first_item.save()
+        saved_items = CVExperience.objects.all()
+        self.assertEqual(saved_items.count(), 1)
+        self.assertEqual(saved_items[0].duration, '2020')
+
+    def test_create_and_read_hard_skill(self):
+        first_item = HardSkill(icon='fas fa-school')
+        first_item.save()
+        saved_items = HardSkill.objects.all()
+        self.assertEqual(saved_items.count(), 1)
+        self.assertEqual(saved_items[0].icon, 'fas fa-school')
+
+    def test_create_and_read_soft_skill(self):
+        first_item = SoftSkill()
+        first_item.text = 'TDD Expert'
+        first_item.save()
+        saved_items = SoftSkill.objects.all()
+        self.assertEqual(saved_items.count(), 1)
+        self.assertEqual(saved_items[0].text, 'TDD Expert')
+
+    def test_create_and_read_project(self):
+        first_item = Project()
+        first_item.title = 'Django App'
+        first_item.description = 'Blog and CV'
+        first_item.duration = 'June 2020'
+        first_item.link = ''
+        first_item.save()
+        saved_items = Project.objects.all()
+        self.assertEqual(saved_items.count(), 1)
+        self.assertEqual(saved_items[0].description, 'Blog and CV')
+
+    def test_create_and_read_interest(self):
+        first_item = Interest(icon='fas fa-palette')
+        first_item.save()
+        saved_items = Interest.objects.all()
+        self.assertEqual(saved_items.count(), 1)
+        self.assertEqual(saved_items[0].icon, 'fas fa-palette')
+
+    def test_create_and_read_language(self):
+        first_item = Language()
+        first_item.name = 'Arabic'
+        first_item.proficiency = 'Noob'
+        first_item.level = 1
+        first_item.save()
+        saved_items = Language.objects.all()
+        self.assertEqual(saved_items.count(), 1)
+        self.assertEqual(saved_items[0].proficiency, 'Noob')
